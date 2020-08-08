dea5fa04db7d80199bd8cbbb59437a6e3c8898e0
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-08 17:22:49 -0500

    adding signup and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Homepage from './pages/Homepage';
-import Profile from './pages/Profile';
-import './App.scss';
-import {BrowserRouter as  Router, Route, Switch} from "react-router-dom";
-
+import React from "react";
+import Homepage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-        <Route exact path={'/'}>
-          <Homepage/>
-        </Route>
-        <Route exact path ={'/profile'}>
-          <Profile/>
-        </Route>
-        <Route>
-          <Homepage/>
-        </Route>
-      </Switch>
-      
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path={"/"}>
+            <Homepage />
+          </Route>
+          <Route exact path={"/profile"}>
+            <Profile />
+          </Route>
+          <Route>
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
