f70cfc3e1eab9712df1669d2914e27656114411d
Valerie Hill <itsvhill@gmail.com>
2020-08-11 15:44:45 -0500

    added two routes (user and reviews)

--- routes/api/review.js
+++ routes/api/review.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
 const reviewController = require("../../controllers/reviewController");
 
-router.route("/")
-    .get(reviewController.findAll)
-    .post(reviewController.create);
-
-router.route("/:id") //if we decide to do a page showing all the reviewed bbq joints?
+router.route("/profile")
     .get(reviewController.findAll)
     .post(reviewController.create);
 
