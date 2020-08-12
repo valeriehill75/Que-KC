18737f02a8b3f79bccabef668ec31671d6cbed40
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-11 20:33:11 -0500

    User Form modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Homepage from "./pages/Homepage";
 import Profile from "./pages/Profile";
-import { Signup } from "./pages/Signup";
-import { Login } from "./pages/Login";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
