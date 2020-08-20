3d10bc6d5eaab814e9468ab026c748eb241c9b9a
Chris Kramer <ChrisKramer15@outlook.com>
2020-08-20 03:51:04 -0500

    back-end authentication functional, still needing to connect with front-end

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const cors = require("cors");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
-const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const User = require("./models/usersModel");
-
-//Defining the port
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Creating the express app
-const app = express();
+//Mongoose connection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/review_db", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +25,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+//NEEDED FOR PASSPORT JS
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -40,56 +45,12 @@ app.use(
 app.use(cookieParser("secretcode"));
 app.use(passport.initialize());
 app.use(passport.session());
-require("./passportConfig")(passport);
-app.post("/api/users/", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send("No User Exists");
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send("Successfully Authenticated");
-        console.log(req.user);
-      });
-    }
-  })(req, res, next);
-});
-app.post("/api/users/signup", (req, res) => {
-  User.findOne({ email: req.body.email }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User already exists");
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+require("./config/passport");
 
-      const newUser = new User({
-        email: req.body.email,
-        password: hashedPassword,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-      });
-      await newUser.save();
-      res.send("User Created!!");
-    }
-  });
-});
-
-//Mongoose connection
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/review_db", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+//----------------------------------------- END OF MIDDLEWARE---------------------------------------------------
 
 //ROUTES
 app.use(routes);
-
-//get user route
-app.get("/user", (req, res) => {
-  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
-});
 
 //Connecting on PORT 3001
 app.listen(PORT, function () {
