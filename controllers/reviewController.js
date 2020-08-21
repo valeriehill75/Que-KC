6dacad3a67c71e23d7abb60d32e7cb19061e538d
Valerie Hill <itsvhill@gmail.com>
2020-08-20 21:09:29 -0500

    linking users and reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,6 @@
 const UserReview = require("../models/reviewModel");
+const UserData = require("../models/usersModel");
+const { ObjectId } = require('mongodb');
 
 //Defining methods
 module.exports = {
@@ -10,6 +12,16 @@ module.exports = {
   create(req, res) {
     const newReview = new UserReview(req.body);
 
-    newReview.save().then(() => res.json("Review added!"));
-  },
+    // newReview.save().then(() => res.json("Review added!"));
+    newReview.save().then((dbReview) => {
+      // return UserData.findOneAndUpdate({ _id: `ObjectId("${req.body.userId}")` }, {$push: {reviews: dbReview._id}}, { new: true });
+      return UserData.findOneAndUpdate({ "_id": new ObjectId(req.body.userId) }, {$push: {reviews: dbReview._id}}, { new: true });
+    })
+      .then(function(dbUser) {
+        res.json(dbUser);
+      })
+      .catch(function(err) {
+        res.json(err);
+      }) 
+  }
 };
